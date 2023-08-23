34a98ffdf2151fcb50dce36549010aed29107cae
samira toubal seghir <samsidt@gmail.com>
2023-08-23 02:04:10 +0200

    Complited Assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+console.log(record.result);
+
+function superbowlWin(arr) {
+    const winningEntry = arr.find(entry => entry.result === 'W');
+    if (winningEntry) {
+      return winningEntry.year;
+    }
+    return undefined;
+  }
+  
+  console.log(superbowlWin(record));
